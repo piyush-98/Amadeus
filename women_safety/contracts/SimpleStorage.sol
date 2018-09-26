388417281a57f02d7a94a8f65778a3559f1cc1bc
iamjaspreetsingh <jsk1961998@gmail.com>
2018-09-27 00:32:57 +0530

    ownership added

--- women_safety/contracts/SimpleStorage.sol
+++ women_safety/contracts/SimpleStorage.sol
@@ -1,13 +1,85 @@
 pragma solidity 0.4.24;
 
 contract SimpleStorage {
-  string ipfsHash;
+string ipfsHash;
+address private _owner;
 
-  function set(string x) public {
+ 
+  event OwnershipRenounced(address indexed previousOwner);
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
+    _owner = msg.sender;
+  }
+
+  /**
+   * @return the address of the owner.
+   */
+  function owner() public view returns(address) {
+    return _owner;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(isOwner());
+    _;
+  }
+
+  /**
+   * @return true if `msg.sender` is the owner of the contract.
+   */
+  function isOwner() public view returns(bool) {
+    return msg.sender == _owner;
+  }
+
+  /**
+   * @dev Allows the current owner to relinquish control of the contract.
+   * @notice Renouncing to ownership will leave the contract without an owner.
+   * It will not be possible to call the functions with the `onlyOwner`
+   * modifier anymore.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipRenounced(_owner);
+    _owner = address(0);
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    _transferOwnership(newOwner);
+  }
+
+  /**
+   * @dev Transfers control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+   
+  function _transferOwnership(address newOwner) internal {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(_owner, newOwner);
+    _owner = newOwner;
+  }
+
+function set(string x) public {
+    
     ipfsHash = x;
-  }
-
-  function get() public view returns (string) {
-    return ipfsHash;
-  }
 }
+
+function get() public view returns (string) {
+    require(isOwner());
+    return ipfsHash;}
+
+
+}
+
